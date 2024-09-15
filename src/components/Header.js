4899febee5f7a5a1bcb92b933947eeaac23e3430
xion20 <davidwwe421@gmail.com>
2024-09-15 17:25:27 -0300

    Fst commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Header.css'
+
+function Header() {
+    return (
+        <>
+            <div className="header">
+                <h1>Tic <span>Tac</span> Toe 🎮</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header;
+
